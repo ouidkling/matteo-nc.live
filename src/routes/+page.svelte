2befd01d21d57bcaabd86a375fcbd348b111880f
Mattéo NADLER CAMPOURCY - Macbook <matteo.nadler.campou@gmail.com>
2024-04-27 22:23:20 +0200

    added github.svg and linkedin.svg to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
-	console.log("hello world");
+	import github from '$lib/assets/svg/github.svg';
+	import linkedin from '$lib/assets/svg/linkedin.svg';
+
 </script>
 
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col justify-center items-center h-screen">
+	<div>
+		<h1 class="text">matteo-nc.live</h1>
+	</div>
+	<div class="flex">
+		<a href="https://github.com/ouidkling" target="_blank" class="github logo">
+			<img src={github} alt="github logo" />
+		</a>
+		<a href="https://linkedin.com/in/matteo-nc" target="_blank" class="linkedin logo">
+			<img src={linkedin} alt="linkedin logo" />
+		</a>
+	</div>
+</div>
 
 <style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+
+	.text {
+		@apply text-4xl font-bold opacity-35;
+	}
+
+	.logo {
+		@apply w-12 h-12 m-4 opacity-35;
+	}
 </style>
